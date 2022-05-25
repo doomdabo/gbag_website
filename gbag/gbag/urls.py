d2dde4dfea1c905ceb772601802be8062081cab6
Bokyung Kwon <bbo1209@naver.com>
2022-05-25 23:47:07 +0900

    gbag 수정

--- gbag/gbag/urls.py
+++ gbag/gbag/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import intro.views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',intro.views.home,name="home"),
+    path('introduction/',intro.views.introduction, name="introduction"),
+    path('predict/',intro.views.predict,name="predict"),
+    path('result/',intro.views.result,name="result"),
+    
 ]
